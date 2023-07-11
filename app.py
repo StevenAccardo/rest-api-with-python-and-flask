5241fff0d6f6fc15f1d43108eb24a1768de8db33
Steven Accardo <accardo.steven@gmail.com>
2023-07-10 20:05:05 -0700

    Updated requirements.txt to leverage sqlalchemy and flask-alchemy library as well as set up the project to use sqlite. Created an Item and Store model and established relationships in those models to each corresponding model to pull either the store data or items data as separate properties. Updated the resources to files to use SQlAlchemy where we have our database interactions for our GET, POST, PUT, and DELETE endpoints for stores and items. Updated the Marshmallow schemas to determine how the endpoints can accept and return requests and responses.

--- app.py
+++ app.py
@@ -1,25 +1,38 @@
+import os
+
 from flask import Flask
 from flask_smorest import Api
+from db import db
 from resources.store import blp as StoreBlueprint
 from resources.item import blp as ItemBlueprint
 
-app = Flask(__name__)
+def create_app(db_url=None):
+    app = Flask(__name__)
 
-# Tells flask to bubble any errors/exceptions in any extensions of flask up to the main app so that we can be notified of them.
-app.config["PROPAGATE_EXCEPTIONS"] = True
-app.config["API_TITLE"] = "Stores REST API"
-app.config["API_VERSION"] = "v1"
-# Tells flask-smorest which version of open API to use
-app.config["OPENAPI_VERSION"] = "3.0.3"
-# Tells flask-smorest where the root of the API is.
-app.config["OPENAPI_URL_PREFIX"] = "/"
-# Tells flask-smorest where to render the swagger ui.
-app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
-# Tells flask-smorest where to download the swagger library from for documenting our API.
-app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    # Tells flask to bubble any errors/exceptions in any extensions of flask up to the main app so that we can be notified of them.
+    app.config["PROPAGATE_EXCEPTIONS"] = True
+    app.config["API_TITLE"] = "Stores REST API"
+    app.config["API_VERSION"] = "v1"
+    # Tells flask-smorest which version of open API to use
+    app.config["OPENAPI_VERSION"] = "3.0.3"
+    # Tells flask-smorest where the root of the API is.
+    app.config["OPENAPI_URL_PREFIX"] = "/"
+    # Tells flask-smorest where to render the swagger ui.
+    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+    # Tells flask-smorest where to download the swagger library from for documenting our API.
+    app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URI", "sqlite:///data.db")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["PROPAGATE_EXCEPTIONS"] = True
+    db.init_app(app)
 
-# Connects flask-smorest with our flask app
-api = Api(app)
+    # Connects flask-smorest with our flask app
+    api = Api(app)
 
-api.register_blueprint(StoreBlueprint)
-api.register_blueprint(ItemBlueprint)
+    with app.app_context():
+        db.create_all()
+
+    api.register_blueprint(StoreBlueprint)
+    api.register_blueprint(ItemBlueprint)
+
+    return app
---
+++ models/store.py
@@ -0,0 +1,9 @@
+from db import db
+
+class StoreModel(db.Model):
+    __tablename__ = "stores"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    # Cascade means that if you delete a store from the DB, the child or related items will be deleted as well.
+    items = db.relationship("ItemModel", back_populates="store", lazy="dynamic", cascade="all, delete")
